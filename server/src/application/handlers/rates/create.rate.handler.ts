367a7146fd974c14701ed61aba55ebef8fbb433b
Santi2411 <santi.gonzalez2411@gmail.com>
2021-10-22 19:46:48 -0300

    create handler rate

--- server/src/application/handlers/rates/create.rate.handler.ts
+++ server/src/application/handlers/rates/create.rate.handler.ts
@@ -2,17 +2,40 @@ import { Rate } from "../../../domain/entities/rate.entity";
 import RateRepository from "../../../infrastructure/repositories/rate.repository";
 import CreateCommandRates from "../../commands/create.command.rates";
 import TechnologyRepository from "../../../infrastructure/repositories/technology.repository";
+import { Technology } from "../../../domain/entities/technology.entity";
 
 class CreateRateHandler {
+
     async execute(command: CreateCommandRates){
-        const rate: Rate = {
-            technology: command.getTechnology(),
-            seniority: command.getSeniority(),
-            language: command.getLanguage(),
-            averageSalary: command.getAverageSalary(),
-            grossMargin: command.getGrossMargin(),
-            currency: command.getCurrency()
-        };
+
+        const technology: Technology | null = await TechnologyRepository.findOneById(command.getTechnology());
+
+        if (!technology) {
+            throw new Error("Technology not found");
+        }
+
+        const existsRate = await RateRepository.exists(
+            technology.getId(),
+            command.getSeniority(), 
+            command.getLanguage(), 
+            command.getCurrency()
+            );
+
+
+        if (!existsRate) {
+            throw new Error("Rate not found");
+        }
+
+
+        const rate: Rate = new Rate(
+            technology, 
+            command.getSeniority(), 
+            command.getLanguage(), 
+            command.getAverageSalary(), 
+            command.getGrossMargin(), 
+            command.getCurrency()
+        );
+
         await RateRepository.save(rate)
     }
 }
