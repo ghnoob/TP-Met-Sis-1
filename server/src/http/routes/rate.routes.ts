6436e9f074c7ed5afc233bda22608d4ed26a1ed1
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-23 00:46:12 -0300

    filter rates route

--- server/src/http/routes/rate.routes.ts
+++ server/src/http/routes/rate.routes.ts
@@ -2,6 +2,7 @@ import { Application } from "express";
 import CommonRoutes from "./common.routes";
 import CreateRateAction from "../actions/create.rate.action";
 import ListRateAction from "../actions/list.rate.action";
+import FilterRateAction from "../actions/filter.rate.action";
 
 
 class RateRoutes extends CommonRoutes {
@@ -19,6 +20,8 @@ class RateRoutes extends CommonRoutes {
             // this.app.put('/rates', UpdateRateAction.run);
     
             // this.app.delete('/rates', DeleteRateAction.run);
+
+            this.app.post('/rates/filter', FilterRateAction.run)
             
     
             return this.app;
