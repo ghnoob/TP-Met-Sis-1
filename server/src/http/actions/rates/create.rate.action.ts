dae7a9a2e97330b797df1d251a022f9fe77bc1fc
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-29 20:07:13 -0300

    fix: change 'User' to 'Rate'

--- server/src/http/actions/rates/create.rate.action.ts
+++ server/src/http/actions/rates/create.rate.action.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import CreateRateHandler from "../../../application/handlers/rates/create.rate.handler";
 import CreateRateCommand from "../../../application/commands/rates/create.rate.command";
 
-class CreateUserAction {
+class CreateRateAction {
     async run(req: Request, res: Response) {
         const command: CreateRateCommand = req.body;
         
         await CreateRateHandler.execute(command);
         
-        return res.status(201).json({message: "User created"});
+        return res.status(201).json({message: "Rate created"});
     }
 }
 
-export default new CreateUserAction();
+export default new CreateRateAction();
