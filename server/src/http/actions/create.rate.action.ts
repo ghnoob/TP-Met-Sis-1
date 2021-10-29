7f65aca0f48a44c0940baa43e5f18493482e41fc
Matías Olmos <matias.olmos.facultad@gmail.com>
2021-10-29 18:22:23 -0300

    cambios hechos durante la clase para que funcione (no funciona)

--- server/src/http/actions/create.rate.action.ts
+++ server/src/http/actions/create.rate.action.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import CreateRateHandler from "../../application/handlers/rates/create.rate.handler";
 import CreateCommandRates from "../../application/commands/create.command.rates";
 
-class CreateUserAction {
+class CreateRateAction {
     async run(req: Request, res: Response) {
         const command: CreateCommandRates = req.body;
         
         await CreateRateHandler.execute(command);
         
-        return res.status(201).json({message: "User created"});
+        return res.status(201).json({message: "Rate created"});
     }
 }
 
-export default new CreateUserAction();
+export default new CreateRateAction();
