b02e15042379d5f55de0d57c4b70e8cbfd97ee48
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-29 22:25:41 -0300

    fix: technology routes

--- server/src/http/routes/technology.routes.ts
+++ server/src/http/routes/technology.routes.ts
@@ -1,30 +1,29 @@
 import { Application } from "express";
 import CreateTechnologyAction from "../actions/technologies/create.technology.action";
-import DeleteTechnologyAction from "../actions/technologies/delete.technology.action";
+// import DeleteTechnologyAction from "../actions/technologies/delete.technology.action";
 import ListTechnologyAction from "../actions/technologies/list.technology.action";
-import UpdateTechnologyAction from "../actions/technologies/update.technology.action";
+// import UpdateTechnologyAction from "../actions/technologies/update.technology.action";
 import CommonRoutes from "./common.routes";
 
 
-class RateRoutes extends CommonRoutes {
+class TechnologyRoutes extends CommonRoutes {
     constructor (app: Application){
-        super(app, "Rate")
+        super(app, "Technologies")
     }
 
-    
-        setUpRoutes(): Application {
 
-            this.app.get('/rates', ListTechnologyAction.run);
-    
-            this.app.post('/rates', CreateTechnologyAction.run);
-    
-            // this.app.put('/rates', UpdateTechnologyAction.run);
-    
-            // this.app.delete('/rates', DeleteTechnologyAction.run);
-            
-    
-            return this.app;
+    setUpRoutes(): Application {
 
-        }
+        this.app.get('/technologies', ListTechnologyAction.run);
+
+        this.app.post('/technologies', CreateTechnologyAction.run);
+
+        // this.app.put('/technologies', UpdateTechnologyAction.run);
+
+        // this.app.delete('/technologies', DeleteTechnologyAction.run);
+
+        return this.app;
+
+    }
 }
-export default RateRoutes;
+export default TechnologyRoutes;
