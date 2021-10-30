d70cc15b01ccbecb8fe2d2acb1fb4bae4dd49f67
ghnoob <ptnchk.rodrigo@gmail.com>
2021-10-30 16:53:14 -0300

    refactor: apply custom classes in handlers

--- server/src/application/handlers/technologies/update.technology.handler.ts
+++ server/src/application/handlers/technologies/update.technology.handler.ts
@@ -1,20 +1,23 @@
 import TechnologyRepository from "../../../infrastructure/repositories/technology.repository";
 import UpdateTechnologyCommand from "../../commands/technologies/update.technology.command";
+import TechnologyAlreadyExistsError from "../../customErrors/technologies/technology.already.exists.error";
+import TechnologyNameNotSpecifiedError from "../../customErrors/technologies/technology.name.not.specified.error";
+import TechnologyNotFoundError from "../../customErrors/technologies/technology.not.found.error";
 
 class UpdateTechnologyHandler{
     async execute(command:UpdateTechnologyCommand){
         const technology = await TechnologyRepository.findOneById(command.getId());
 
         if (!technology) {
-            throw new Error("Not found");
+            throw new TechnologyNotFoundError();
         }
 
         if (!command.getName()) {
-            throw new Error("Name not specified");
+            throw new TechnologyNameNotSpecifiedError();
         }
 
         if (await TechnologyRepository.findOneByName(command.getName())) {
-            throw new Error("Technology already exists")
+            throw new TechnologyAlreadyExistsError();
         }
 
         technology.setName(command.getName());
