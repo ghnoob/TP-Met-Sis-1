c23c7345e77de5b79215eff6cc53dc99401cd5de
Santi2411 <santi.gonzalez2411@gmail.com>
2021-10-31 23:51:46 -0300

    commmand correction

--- server/src/http/actions/rates/create.rate.action.ts
+++ server/src/http/actions/rates/create.rate.action.ts
@@ -4,7 +4,13 @@ import CreateRateCommand from "../../../application/commands/rates/create.rate.c
 
 class CreateRateAction {
     async run(req: Request, res: Response) {
-        const command: CreateRateCommand = req.body;
+        const command: CreateRateCommand = new CreateRateCommand(
+            req.body.technology,
+            req.body.seniority,
+            req.body.language,
+            req.body.averageSalary,
+            req.body.grossMargin,
+            req.body.currency);
         
         await CreateRateHandler.execute(command);
         
