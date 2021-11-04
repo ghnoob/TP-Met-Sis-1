b8ea51fa4083c8467603cc4512de976502273058
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-04 13:50:48 -0300

    add try/catch block in action

--- server/src/http/actions/rates/create.rate.action.ts
+++ server/src/http/actions/rates/create.rate.action.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from "express";
 import CreateRateHandler from "../../../application/handlers/rates/create.rate.handler";
 import CreateRateCommand from "../../../application/commands/rates/create.rate.command";
+import type ApplicationError from "../../../application/customErrors/application.error";
 
 class CreateRateAction {
     async run(req: Request, res: Response) {
-        const command: CreateRateCommand = new CreateRateCommand(
-            req.body.technology,
-            req.body.seniority,
-            req.body.language,
-            req.body.averageSalary,
-            req.body.grossMargin,
-            req.body.currency);
-        
-        await CreateRateHandler.execute(command);
-        
-        return res.status(201).json({message: "Rate created"});
+        try {
+            const command: CreateRateCommand = new CreateRateCommand(
+                req.body.technology,
+                req.body.seniority,
+                req.body.language,
+                req.body.averageSalary,
+                req.body.grossMargin,
+                req.body.currency
+            );
+            await CreateRateHandler.execute(command);
+            return res.status(201).json({message: "Rate created"});
+        } catch (error) {
+            const err: ApplicationError = error as ApplicationError;
+            return res.status(err.status).json({ message: err.message });
+        }
     }
 }
 
