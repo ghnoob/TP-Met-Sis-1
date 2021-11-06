c60546ab9295162e2268c636ee4c1877acaa4c94
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-06 00:45:01 -0300

    add validation in actions

--- server/src/http/actions/rates/create.rate.action.ts
+++ server/src/http/actions/rates/create.rate.action.ts
@@ -2,9 +2,16 @@ import { Request, Response } from "express";
 import CreateRateHandler from "../../../application/handlers/rates/create.rate.handler";
 import CreateRateCommand from "../../../application/commands/rates/create.rate.command";
 import type ApplicationError from "../../../application/customErrors/application.error";
+import { validationResult } from "express-validator";
 
 class CreateRateAction {
     async run(req: Request, res: Response) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
         try {
             const command: CreateRateCommand = new CreateRateCommand(
                 req.body.technology,
