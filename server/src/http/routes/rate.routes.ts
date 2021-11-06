bbaab1c5cc1869f8388752c9b0da11fe72f2e07f
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-06 00:42:42 -0300

    add validation to rate routes

--- server/src/http/routes/rate.routes.ts
+++ server/src/http/routes/rate.routes.ts
@@ -5,7 +5,10 @@ import ListRateAction from "../actions/rates/list.rate.action";
 import FilterRateAction from "../actions/rates/filter.rate.action";
 import updateRateAction from "../actions/rates/update.rate.action";
 import deleteRateAction from "../actions/rates/delete.rate.action";
-
+import { body } from "express-validator";
+import { CurrencyEnum } from "../../domain/enums/currency.enum";
+import { LanguageEnum } from "../../domain/enums/language.enum";
+import { SeniorityEnum } from "../../domain/enums/seniority.enum";
 
 class RateRoutes extends CommonRoutes {
     constructor (app: Application){
@@ -16,13 +19,52 @@ class RateRoutes extends CommonRoutes {
 
             this.app.get('/rates', ListRateAction.run);
     
-            this.app.post('/rates', CreateRateAction.run);
-    
-            this.app.put('/rates', updateRateAction.run);
+            this.app.post(
+                '/rates',
+
+                body('technology', 'value must not be empty').trim().notEmpty(),
+
+                body(
+                    'seniority',
+                    `invalid value. Allowed: ${Object.values(SeniorityEnum).join(', ')}`
+                ).trim().toLowerCase().isIn(Object.values(SeniorityEnum)),
+
+                body(
+                    'language',
+                    `invalid value. Allowed: ${Object.values(LanguageEnum).join(', ')}`
+                ).trim().toLowerCase().isIn(Object.values(LanguageEnum)),
+
+                body(
+                    ['averageSalary', 'grossMargin'],
+                    'value must be a numeric string, positive, up to 2 decimal places',
+                ).isString().trim().matches(/^\d+(\.\d{1,2})?$/),
+
+                body(
+                    'currency',
+                    `invalid value. Allowed: ${Object.values(CurrencyEnum).join(', ')}`
+                ).trim().toUpperCase().isIn(Object.values(CurrencyEnum)),
+
+                CreateRateAction.run,
+            );
+
+            this.app.put(
+                '/rates',
+                body(
+                    ['averageSalary', 'grossMargin'],
+                    'value must be a numeric string, positive, up to 2 decimal places',
+                ).isString().trim().matches(/^\d+(\.\d{1,2})?$/),
+
+                updateRateAction.run,
+            );
     
             this.app.delete('/rates', deleteRateAction.run);
 
-            this.app.post('/rates/filter', FilterRateAction.run)
+            this.app.post(
+                '/rates/filter',
+                body(['seniority', 'language']).trim().toLowerCase(),
+                body('currency').trim().toUpperCase(),
+                FilterRateAction.run
+            )
             
     
             return this.app;
