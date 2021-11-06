4b11b4b69313fb930b7c834b9b6515742b94506e
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-06 17:27:24 -0300

    reneme class

--- server/src/application/handlers/rates/create.rate.handler.ts
+++ server/src/application/handlers/rates/create.rate.handler.ts
@@ -4,14 +4,14 @@ import CreateRateCommand from "../../commands/rates/create.rate.command";
 import TechnologyRepository from "../../../infrastructure/repositories/technology.repository";
 import { Technology } from "../../../domain/entities/technology.entity";
 import RateAlreadyExistsError from "../../customErrors/rates/rate.already.exists.error";
-import RateTechnologyIdInvalid from "../../customErrors/rates/rate.technology.id.invalid";
+import RateTechnologyIdNotValidError from "../../customErrors/rates/rate.technology.id.not.valid.error";
 
 class CreateRateHandler {
     async execute(command: CreateRateCommand) {
         const technology: Technology | null = await TechnologyRepository.findOneById(command.getTechnology());
 
         if (!technology) {
-            throw new RateTechnologyIdInvalid();
+            throw new RateTechnologyIdNotValidError();
         }
 
         const existsRate = await RateRepository.exists(
