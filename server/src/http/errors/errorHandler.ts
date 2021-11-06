f029a81455f84123e38253c8c43cd12adc4efc14
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-06 17:19:52 -0300

    create error handling middleware

---
+++ server/src/http/errors/errorHandler.ts
@@ -0,0 +1,8 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import statusCodeMapper from "./statusCodeMapper";
+
+const errorHandler: ErrorRequestHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
+    return res.status(statusCodeMapper(err.name)).json(err);
+}
+
+export default errorHandler;
---
+++ server/src/http/errors/statusCodeMapper.ts
@@ -0,0 +1,16 @@
+import { StatusCodes } from "./statusCodes";
+
+const statusCodeMap = new Map<string, number>([
+    ['ApplicationError', StatusCodes.InternalServerError],
+    ['RateAlreadyExistsError', StatusCodes.UnproccessableEntity],
+    ['RateNotFoundError', StatusCodes.NotFound],
+    ['RateTechnologyIdNotValidError', StatusCodes.BadRequest],
+    ['TechnologyAlreadyExistsError', StatusCodes.UnproccessableEntity],
+    ['TechnologyNotFoundError', StatusCodes.NotFound],
+    ['SyntaxError', StatusCodes.BadRequest],
+    ['Error', StatusCodes.BadRequest], // validation error
+]);
+
+export default function statusCodeMapper(errorName: string): number {
+    return statusCodeMap.get(errorName) ?? StatusCodes.InternalServerError;
+}
