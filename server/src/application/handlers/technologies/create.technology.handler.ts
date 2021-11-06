821cc12fbb1128f254c40397b6c4a0d0560046f3
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-05 23:29:39 -0300

    remove unnecesary validation from handler

--- server/src/application/handlers/technologies/create.technology.handler.ts
+++ server/src/application/handlers/technologies/create.technology.handler.ts
@@ -2,16 +2,9 @@ import { Technology } from "../../../domain/entities/technology.entity";
 import TechnologyRepository from "../../../infrastructure/repositories/technology.repository";
 import CreateTechnologyCommand from "../../commands/technologies/create.technology.command";
 import TechnologyAlreadyExistsError from "../../customErrors/technologies/technology.already.exists.error";
-import TechnologyNameNotSpecifiedError from "../../customErrors/technologies/technology.name.not.specified.error";
-
 
 class CreateTechologyHandler {
-
     async execute(command: CreateTechnologyCommand){
-        if (!command.getName()) {
-            throw new TechnologyNameNotSpecifiedError();
-        }
-
         if (await TechnologyRepository.findOneByName(command.getName())){
             throw new TechnologyAlreadyExistsError();
         }
@@ -19,8 +12,6 @@ class CreateTechologyHandler {
         const technology : Technology = new Technology(command.getName());
 
         await TechnologyRepository.save(technology);
-
-
     }
 }
 export default new CreateTechologyHandler();
