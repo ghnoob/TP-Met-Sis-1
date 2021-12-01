ee10b776f940b462b4f7f675bc4404a5b21b8003
ghnoob <ptnchk.rodrigo@gmail.com>
2021-12-01 20:10:15 -0300

    document UpdateRateCommand

--- server/src/application/commands/rates/update.rate.command.ts
+++ server/src/application/commands/rates/update.rate.command.ts
@@ -1,3 +1,19 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UpdateRateCommand:
+ *       type: object
+ *       properties:
+ *         averageSalary:
+ *           type: string
+ *           description: Average salary of the devs with this rate, as a numeric string
+ *           example: '75000'
+ *         grossMargin:
+ *           type: string
+ *           description: Gross margin taken as a retribution to the community. Usually it's 1% of the salary
+ *           example: '750'
+ */
 export default class UpdateRateCommand {
   private id: string;
   private averageSalary: string;
