70aa35ad521287c55ad39b55b9851b099e0eadf3
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-30 23:36:47 -0300

    standarize error messages

--- server/src/http/errors/errorHandler.ts
+++ server/src/http/errors/errorHandler.ts
@@ -3,7 +3,10 @@ import statusCodeMapper from './statusCodeMapper';
 
 // eslint-disable-next-line
 const errorHandler: ErrorRequestHandler = (err: any, _req: Request, res: Response, _next: NextFunction) => {
-  return res.status(statusCodeMapper(err.name)).json(err);
+  const statusCode: number = statusCodeMapper(err.name);
+
+  // los errores de validacion tiene errors y los demas message
+  return res.status(statusCode).json({ statusCode, message: err.errors ?? err.message });
 };
 
 export default errorHandler;
