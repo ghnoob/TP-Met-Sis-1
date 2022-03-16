e23dd966ff3429ff99058c237a9370d8127c3f3e
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-15 21:57:53 -0300

    inject technology handler into actions

--- server/src/http/actions/technologies/create.technology.action.ts
+++ server/src/http/actions/technologies/create.technology.action.ts
@@ -1,17 +1,23 @@
-import { NextFunction, Request, Response } from 'express';
-import { Service } from 'typedi';
+import type { NextFunction, Request, Response } from 'express';
+import { Inject, Service } from 'typedi';
 import CreateTechnologyCommand from '../../../application/commands/technologies/create.technology.command';
 import CreateTechnologyHandler from '../../../application/handlers/technologies/create.technology.handler';
 import type { Technology } from '../../../domain/entities/technology.entity';
-import ActionInterface from '../../../domain/interfaces/action.interface';
+import type ActionInterface from '../../../domain/interfaces/action.interface';
+import type HandlerInterface from '../../../domain/interfaces/handler.interface';
 
 @Service()
 export default class CreateTechnologyAction implements ActionInterface {
+  constructor(
+    @Inject(() => CreateTechnologyHandler)
+    private readonly handler: HandlerInterface<Technology>,
+  ) {}
+
   async run(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
     try {
       const command: CreateTechnologyCommand = new CreateTechnologyCommand(req.body.name);
 
-      const technology: Technology = await CreateTechnologyHandler.execute(command);
+      const technology: Technology = await this.handler.execute(command);
 
       return res.status(201).json(technology);
     } catch (error) {
--- server/src/http/actions/technologies/delete.technology.action.ts
+++ server/src/http/actions/technologies/delete.technology.action.ts
@@ -1,16 +1,22 @@
-import { NextFunction, Request, Response } from 'express';
-import { Service } from 'typedi';
-import ActionInterface from '../../../domain/interfaces/action.interface';
+import type { NextFunction, Request, Response } from 'express';
+import { Inject, Service } from 'typedi';
+import type ActionInterface from '../../../domain/interfaces/action.interface';
 import DeleteTechnologyCommand from '../../../application/commands/technologies/delete.technology.command';
 import DeleteTechnologyHandler from '../../../application/handlers/technologies/delete.technology.handler';
+import type HandlerInterface from '../../../domain/interfaces/handler.interface';
 
 @Service()
 export default class DeleteTechnologyAction implements ActionInterface {
+  constructor(
+    @Inject(() => DeleteTechnologyHandler)
+    private readonly handler: HandlerInterface<void>,
+  ) {}
+
   async run(req: Request, res: Response, next: NextFunction): Promise<void | Response> {
     const command: DeleteTechnologyCommand = new DeleteTechnologyCommand(req.params.id);
 
     try {
-      await DeleteTechnologyHandler.execute(command);
+      await this.handler.execute(command);
 
       return res.status(204).end();
     } catch (error) {
