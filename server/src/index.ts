4e9d9a7c1593afc8ab029bd24d8fce8a487adc81
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-16 23:10:40 -0300

    add typeorm boilerplate

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,18 @@
 import 'reflect-metadata';
 import { Container } from 'typedi';
+import { createConnection, useContainer } from 'typeorm';
 import Application from './config/application';
+import logger from './infrastructure/logger';
 
-const app = Container.get(Application);
+useContainer(Container);
 
-app.bootstrap();
+createConnection()
+  .then(() => {
+    logger.info('Database connected');
+
+    Container.get(Application).bootstrap();
+  })
+  .catch(error => {
+    logger.error("Couldn't connect to the database");
+    logger.error(error.message);
+  });
