bf6e51e9b822fe6f62cb0d20b2d2e54d9488c93e
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-16 23:31:05 -0300

    mark technology as a db entity

--- server/src/domain/entities/technology.entity.ts
+++ server/src/domain/entities/technology.entity.ts
@@ -1,4 +1,12 @@
-import shortid from 'shortid';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Index,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
 /**
  * @swagger
@@ -11,21 +19,37 @@ import shortid from 'shortid';
  *         - name
  *       properties:
  *         id:
- *           type: string
+ *           type: number
  *           readOnly: true
  *           description: The technology ID.
- *           example: eWRhpRV
+ *           example: 1
  *         name:
  *           type: string
  *           description: The technology name.
  *           example: javascript
  */
+/**
+ * Represents a technology that a rate can belong to.
+ */
+@Entity()
 export default class Technology {
-  private id: string;
+  @PrimaryGeneratedColumn()
+  private id?: number;
+
+  @Index({ unique: true })
+  @Column()
   private name: string;
 
+  @CreateDateColumn()
+  private createdAt?: Date;
+
+  @UpdateDateColumn()
+  private updatedAt?: Date;
+
+  @DeleteDateColumn({ select: false })
+  private deletedAt?: Date;
+
   constructor(name: string) {
-    this.id = shortid.generate();
     this.name = name;
   }
 
@@ -36,7 +60,20 @@ export default class Technology {
   public getName() {
     return this.name;
   }
+
   public setName(name: string) {
     this.name = name;
+  }
+
+  public getCreationDate() {
+    return this.createdAt;
+  }
+
+  public getUpdateDate() {
+    return this.updatedAt;
+  }
+
+  public getDeletionDate() {
+    return this.deletedAt;
   }
 }
