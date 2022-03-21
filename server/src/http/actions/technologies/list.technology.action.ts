00dcf2fd6ee16745e3454e01488eed6af167beba
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-21 19:48:46 -0300

    inject repository in list actions

--- server/src/http/actions/technologies/list.technology.action.ts
+++ server/src/http/actions/technologies/list.technology.action.ts
@@ -1,12 +1,21 @@
 import { Request, Response } from 'express';
 import { Service } from 'typedi';
+import { InjectRepository } from 'typeorm-typedi-extensions';
 import ActionInterface from '../../../domain/interfaces/action.interface';
-import technologyRepository from '../../../infrastructure/repositories/technology.repository';
+import TechnologyRepository from '../../../infrastructure/repositories/technology.repository';
 
+/**
+ * Middleware for listing all the technologies.
+ */
 @Service()
 export default class ListTechnologyAction implements ActionInterface {
+  constructor(
+    @InjectRepository(TechnologyRepository)
+    private readonly repository: TechnologyRepository,
+  ) {}
+
   async run(_req: Request, res: Response): Promise<Response> {
-    const technologies = await technologyRepository.findAll();
+    const technologies = await this.repository.find();
 
     return res.status(200).json(technologies);
   }
