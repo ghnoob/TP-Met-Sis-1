77eb1aee6df58d9c7e1c149f0f7557043b051ff4
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-21 22:16:52 -0300

    document logger

--- server/src/infrastructure/logger/index.ts
+++ server/src/infrastructure/logger/index.ts
@@ -1,6 +1,9 @@
 import { createLogger } from 'winston';
 import loggerOptions from '../../config/logger.config';
 
+/**
+ * Winston logger.
+ */
 const logger = createLogger({ ...loggerOptions, level: 'info' });
 
 export default logger;
