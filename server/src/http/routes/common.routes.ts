646ba23054bee5b6d5b925669c37405b932bc32b
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-21 22:15:42 -0300

    add jsdoc to routes

--- server/src/http/routes/common.routes.ts
+++ server/src/http/routes/common.routes.ts
@@ -4,18 +4,31 @@ export default abstract class CommonRoutes {
   private router: Router;
   private name: string;
 
+  /**
+   * Creates a new route.
+   * @param name Route endpoint.
+   */
   constructor(name: string) {
     this.router = Router();
     this.name = name;
   }
 
+  /**
+   * Gets route name.
+   */
   public getName() {
     return this.name;
   }
 
+  /**
+   * Gets router.
+   */
   public getRouter() {
     return this.router;
   }
 
+  /**
+   * Assigns routes to the router.
+   */
   protected abstract setUpRoutes(): Router;
 }
