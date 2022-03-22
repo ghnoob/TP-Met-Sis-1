c67dbf0f8e489566d010ded35eb44e0e63d59756
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-21 22:04:26 -0300

    add jsdoc for interfaces

--- server/src/domain/interfaces/action.interface.ts
+++ server/src/domain/interfaces/action.interface.ts
@@ -1,5 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 
+/**
+ * Express middleware.
+ */
 export default interface ActionInterface {
+  /**
+   * Express middleware function.
+   */
   run(req: Request, res: Response, next: NextFunction): Promise<Response | void>;
 }
--- server/src/domain/interfaces/handler.interface.ts
+++ server/src/domain/interfaces/handler.interface.ts
@@ -1,3 +1,11 @@
+/**
+ * Handles the logic of a request.
+ */
 export default interface HandlerInterface<T> {
+  /**
+   * Handles the request.
+   * @param command Data needed to handle the request.
+   * @returns Request result.
+   */
   execute(command?: unknown): Promise<T>;
 }
