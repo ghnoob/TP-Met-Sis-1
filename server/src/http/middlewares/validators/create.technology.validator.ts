0742c3b3c20d5e66c03a7c6fdc054218fce0670e
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-21 22:12:31 -0300

    document validators

--- server/src/http/middlewares/validators/create.technology.validator.ts
+++ server/src/http/middlewares/validators/create.technology.validator.ts
@@ -1,5 +1,8 @@
 import { body, ValidationChain } from 'express-validator';
 
+/**
+ * Adds validation info to the request when a technology is created.
+ */
 const createTechnologyValidator: ValidationChain[] = [
   body('name', 'value must not be empty').trim().toLowerCase().notEmpty(),
 ];
--- server/src/http/middlewares/validators/filter.rate.validator.ts
+++ server/src/http/middlewares/validators/filter.rate.validator.ts
@@ -1,5 +1,8 @@
 import { body, ValidationChain } from 'express-validator';
 
+/**
+ * Sanitizes request body.
+ */
 const filterRateSanitizer: ValidationChain[] = [
   body(['seniority', 'language']).trim().toLowerCase(),
   body('currency').trim().toUpperCase(),
