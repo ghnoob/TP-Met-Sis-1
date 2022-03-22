3367b37b3ca9dacaac94e50b1dac8f1ebe69470e
ghnoob <ptnchk.rodrigo@gmail.com>
2022-03-21 22:13:44 -0300

    add jsdoc to middlewares

--- server/src/http/middlewares/logger.middleware.ts
+++ server/src/http/middlewares/logger.middleware.ts
@@ -1,6 +1,12 @@
 import { logger, errorLogger as errLogger } from 'express-winston';
 import loggerOptions from '../../config/logger.config';
 
+/**
+ * Middleware for logging HTTP requests.
+ */
 export const requestLogger = logger(loggerOptions);
 
+/**
+ * Middleware for logging errors.
+ */
 export const errorLogger = errLogger(loggerOptions);
