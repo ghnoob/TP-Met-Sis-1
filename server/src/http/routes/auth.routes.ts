fd724d5ad21f8d1a78556f15e01bdeff0a27ac63
ghnoob <ptnchk.rodrigo@gmail.com>
2022-04-01 16:43:40 -0300

    add login route

--- server/src/http/routes/auth.routes.ts
+++ server/src/http/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { Inject, Service } from 'typedi';
 import ActionInterface from '../../domain/interfaces/action.interface';
+import LoginAction from '../actions/auth/login.action';
 import SignupAction from '../actions/auth/signup.action';
 import validate from '../middlewares/validator.middleware';
 import signupValidator from '../middlewares/validators/signup.validator';
@@ -59,6 +60,50 @@ import CommonRoutes from './common.routes';
  *               statusCode: 409
  *               name: Conflict
  *               message: This email is already registered with another account.
+ * /auth/login:
+ *   post:
+ *     summary: Logs in with an already created account.
+ *     tags: [Auth]
+ *     requestBody:
+ *       description: Longin credentials
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginCommand'
+ *     responses:
+ *       200:
+ *         description: Successful login.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   readOnly: true
+ *                   description: The generated JSON Web Token
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MX0.amQMkQV_5SLABZpVALQoqE1xr1MfujMtaoJIqrNfNbg
+ *       400:
+ *         description: Incorrect email.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *             example:
+ *               statusCode: 400
+ *               name: Bad Request
+ *               message: Incorrect email.
+ *       401:
+ *         descrption: Incorrect password.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ErrorResponse'
+ *             example:
+ *               statusCode: 401
+ *               name: Unauthorized
+ *               message: Incorrect password.
  */
 /**
  * Authentication endpoints.
@@ -68,6 +113,9 @@ export default class AuthRoutes extends CommonRoutes {
   constructor(
     @Inject(() => SignupAction)
     private readonly signupAction: ActionInterface,
+
+    @Inject(() => LoginAction)
+    private readonly loginAction: ActionInterface,
   ) {
     super('/auth');
     this.setUpRoutes();
@@ -76,6 +124,8 @@ export default class AuthRoutes extends CommonRoutes {
     this.getRouter().post('/signup', signupValidator, validate, (req: Request, res: Response, next: NextFunction) =>
       this.signupAction.run(req, res, next),
     );
+
+    this.getRouter().post('/login', (req, res, next) => this.loginAction.run(req, res, next));
 
     return this.getRouter();
   }
