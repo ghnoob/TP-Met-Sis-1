7c09d7243db6f7f627fc90391115c7b4b6cc21eb
ghnoob <ptnchk.rodrigo@gmail.com>
2022-04-04 23:03:06 -0300

    call error handler from validation middleware
    this was done to be able to log the error

--- server/src/http/middlewares/validator.middleware.ts
+++ server/src/http/middlewares/validator.middleware.ts
@@ -1,20 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
-import HttpError from '../errors/http.error';
+import ValidationError from '../../application/errors/validation/validation.error';
 
 /**
  * Middleware for validating request body.
  */
-export default function validate(req: Request, res: Response, next: NextFunction) {
+export default function validate(req: Request, _res: Response, next: NextFunction): void {
   const errors = validationResult(req);
 
   if (errors.isEmpty()) {
     return next();
   }
 
-  const message = errors.array();
-
-  const statusCode = 400;
-
-  return res.status(statusCode).json(new HttpError(statusCode, message));
+  return next(new ValidationError(errors.array()));
 }
