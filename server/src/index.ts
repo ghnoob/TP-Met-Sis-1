c7953645a8b6447580b5f4e7df21653480fc5670
ghnoob <ptnchk.rodrigo@gmail.com>
2022-04-04 22:57:04 -0300

    fix: configure dotenv before importing config file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import 'reflect-metadata';
 import { config } from 'dotenv';
+
+config();
+
 import { Container } from 'typedi';
 import { createConnection, useContainer } from 'typeorm';
 import ExpressService from './domain/services/express.service';
 import logger from './infrastructure/logger';
 import options from './config';
-
-config();
 
 useContainer(Container);
 
